94c5392372881d00d7133af2fbdab233b42dfe79
gaga930 <37590548+gaga930@users.noreply.github.com>
2019-06-15 14:29:23 +0800

    Delete 3_titanic_testing.py

--- WebSite/titanic_prediction_example/3_titanic_testing.py
+++
@@ -1,8 +0,0 @@
-import requests
-import pandas as pd
-
-url = 'http://bdse52:6666/predict'
-data = {'Age':77, 'Embarked':'S', 'Sex':'male'}
-
-response = requests.post(url, data=data)
-response.json()
